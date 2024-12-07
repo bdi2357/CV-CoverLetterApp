fed505ba0bef07646a607c7630625e113af7918e
Itay Ben Dan <itaybd@gmail.com>
2024-12-07 20:33:06 +0200

    fixing app.py printings

--- app.py
+++ app.py
@@ -10,14 +10,7 @@ import os
 from CoverLetterGen.ai_interaction import OpenAIModel, CoverLetterGenerator
 from CoverLetterGen.basic_iterative import BasicIterativeAgent
 
-"""
-if "OPENAI_API_KEY" in st.secrets:
-    st.write("API key loaded successfully.")  # This will be shown in the app
-    print(f"OPENAI_API_KEY: {st.secrets['OPENAI_API_KEY']}")  # This will be printed in the logs
-else:
-    st.write("OPENAI_API_KEY not found in secrets.")  # If the key is not found
-    print("OPENAI_API_KEY not found.")
-"""
+
 # Verify the version of the OpenAI library
 #st.write(f"OpenAI library version: {openai.__version__}")  # Shows in the app
 #print(f"OpenAI library version: {openai.__version__}")  # Shows in the logs
